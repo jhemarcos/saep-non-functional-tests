b03aac83eb3a45a0697d44d25bac3028fddf7898
Jean Marcos <jean_marcos27@hotmail.com>
2016-11-22 21:15:21 -0200

    Added jmeter maven puglin.

---
+++ saep-jmeter/src/main/java/core/Saep.java
@@ -0,0 +1,19 @@
+package core;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jean_
+ */
+public class Saep {
+    
+    public static void main(String[] args){
+        System.out.println("Run it");
+    }
+    
+}
